3c96ee201d176dfa31fe7084a41d5809b2841e18
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-07-23 22:49:42 +0300

    app

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode basename="/goit-react-hw-05-movies">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
